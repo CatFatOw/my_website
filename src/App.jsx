2886a210215c720056bc47821257623c3c8e5ec2
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2024-08-14 00:30:48 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,16 @@ import About from './About/About';
 import Achievement from './Achievement/Achievement';
 import Facts from './FastFacts/Facts';
 
-
 function App() {
   return (
     <Router>
       <div className='App'>
         <NavBar />
-        
-      
         <Routes>
           <Route path='/' element={<><Hero /><About /></>} />
           <Route path='/achievement' element={<Achievement />} />
           <Route path='/facts' element={<Facts />} />
         </Routes>
-        
-       
       </div>
     </Router>
   );
