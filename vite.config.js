af1db4889dfb5974d4ce588ea2129782020babe9
CatFatOw <michaelwufluffy@gmail.com>
2024-08-13 23:48:13 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my_website/', // Update this to match your new repository name
+  base: '/my_website/', // Update this to your repository name
   plugins: [react()],
 });
